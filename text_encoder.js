101197505a718f1776ed238a8eb8fda342a9c005
kouhei-1 <140941081+kouhei-1@users.noreply.github.com>
2024-12-04 11:30:36 +0900

    Update text_encoder.js

--- text_encoder.js
+++ text_encoder.js
@@ -9,9 +9,15 @@ function encodeText() {
     break;
     
     case 'utf-8':
-      result = escape(textToEncode);
-      break;
-    
+      function customEncodeURIComponent(str) {
+       return str.split('')
+         .map(char => '%' + char.charCodeAt(0).toString(16).toUpperCase())
+         .join('');
+      }
+
+      result = customEncodeURIComponent(textToEncode);
+    break;
+      
     case 'utf-16':
       // 文字列を\uXXXX形式にエンコード
       function encodeToUnicodeEscape(str) {
